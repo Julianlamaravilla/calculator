e4c03f850aff1bb8e10db149c4530d598ad09320
julianlamaravilla <julianrestrepo012@gmail.com>
2026-03-22 20:38:18 -0500

    second version

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,12 @@
+import javax.swing.*;
 import java.util.InputMismatchException;
-import java.util.Scanner;
+
 
 public class Calculator {
 
     // main function
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+
 
         // initialized values
         int number1 = 0;
@@ -15,35 +16,41 @@ public class Calculator {
         // message to print with calculator options
         String message = "Please select a option\n1. Add \n2. rest\n3. Multiplication \n4. Division";
 
-        System.out.println(message);
+
 
         try {
-            option = scanner.nextInt();
-            System.out.println("Please Enter first number");
-            number1 = scanner.nextInt();
-            System.out.println("Now, enter second one");
-            number2 = scanner.nextInt();
+            option = Integer.parseInt(JOptionPane.showInputDialog(message));
+
+            // exception logical
+            if (option > 4 || option < 1 ){
+                JOptionPane.showMessageDialog(null,"Error, please enter a value in range");
+                main(args);
+                System.exit(0);
+            }
+            number1 = Integer.parseInt(JOptionPane.showInputDialog("Please Enter first number"));
+            number2 = Integer.parseInt(JOptionPane.showInputDialog("Now, enter second one"));
         } catch (NumberFormatException e){ // Capture the NumberFormatException error
-            System.out.println("Error, please enter a right value" );
+            JOptionPane.showMessageDialog(null,"Error :"+ e +", please enter a right value");
             main(args);
             System.exit(0);
         } catch (InputMismatchException e) { // Capture the InputMismatchException error
-            System.out.println("Error, please enter a right value" );
+            JOptionPane.showMessageDialog(null,"Error :  "+ e +", please enter a right value");
             main(args);
             System.exit(0);
         }
 
         // Logic of calculator and print result
         if (option == 1) {
-            System.out.println("Result: " + number1 + " + " + number2 + " = "+  (number1 + number2));
+            JOptionPane.showMessageDialog(null, "Result: " + number1 + " + " + number2 + " = "+  (number1 + number2));
         } else if (option == 2) {
-            System.out.println("Result: " + number1 + " - " + number2 + " = "+  (number1 - number2));
+            JOptionPane.showMessageDialog(null, "Result: " + number1 + " - " + number2 + " = "+  (number1 - number2));
         } else if (option == 3) {
-            System.out.println("Result: " + number1 + " * " + number2 + " = "+  (number1 * number2));
+            JOptionPane.showMessageDialog(null, "Result: " + number1 + " * " + number2 + " = "+  (number1 * number2));
         } else if (option == 4) {
-            System.out.println("Result: " + number1 + " / " + number2 + " = "+  (number1 / number2));
+            JOptionPane.showMessageDialog(null, "Result: " + number1 + " / " + number2 + " = "+  (number1 / number2));
         } else {
-            System.out.println("Sorry, undefined selection ");
+            JOptionPane.showMessageDialog(null, "Sorry, undefined selection ");
         }
     }
+
 }
